27682d5bcf4b72d0b1f0fb1eb6d0e638e18bc245
Bharat Kathi <bharat1031@gmail.com>
2019-08-23 17:08:23 -0700

    Initial commit

---
+++ src/main/java/com/bk1031/wbdatabase/WbDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.bk1031.wbdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bk1031/wbdatabase/WbDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bk1031.wbdatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WbDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
