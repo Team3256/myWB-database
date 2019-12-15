03659a8be2b2b3ca7953392a0d95cb77db4c3677
Bharat Kathi <bharat1031@gmail.com>
2019-12-15 11:53:32 -0800

    Create Constants.java

---
+++ src/main/java/com/bk1031/wbdatabase/Constants.java
@@ -0,0 +1,6 @@
+public class Constants {
+    final static String url = "jdbc:postgresql://localhost:5432/wb-database";
+    final static String user = "user";
+    final static String password = "password";
+    final static String initPath = "/src/main/resources/init.sql";
+}
